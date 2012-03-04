c46941cd3c2ace76be35d972cb1157657c84c612
peter hausel <peter_kovac11@hotmail.com>
2012-03-04 13:33:18 -0500

    make sure InjectPlugin is referenced in Global (samples)

--- inject/sample/app/Global.java
+++ inject/sample/app/Global.java
@@ -3,12 +3,12 @@ import play.libs.*;
 
 import java.util.*;
 
-import service.Service;
+import service.MyService;
 import com.typesafe.plugin.inject.InjectPlugin;
 
 public class Global extends GlobalSettings {
     
     public void onStart(Application app) {
-      Logger.warn("getting an instance from guice:"+ app.plugin(InjectPlugin.class).getInstance(Service.class));
+      Logger.warn("getting an instance from guice:"+ app.plugin(InjectPlugin.class).getInstance(MyService.class));
     }
 }
