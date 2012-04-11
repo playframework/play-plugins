5a18214859605a5bba0a730fbf293b8acff8a70b
peter hausel <peter_kovac11@hotmail.com>
2012-04-11 11:22:42 -0400

    hot fix release 2.0.1

--- inject/sample/project/Build.scala
+++ inject/sample/project/Build.scala
@@ -8,7 +8,7 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-       "com.typesafe" % "play-plugins-inject" % "2.0-RC4"
+       "com.typesafe" % "play-plugins-inject" % "2.0.1"
       
     )
 
