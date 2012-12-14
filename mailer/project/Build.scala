4aad6051b71d5739350cd5996812828aa25f2f7f
James Roper <james@jazzy.id.au>
2012-12-14 16:02:39 +1100

    Updated mailer plugin to 2.1-RC1

--- mailer/project/Build.scala
+++ mailer/project/Build.scala
@@ -3,8 +3,8 @@ import Keys._
 
 object MinimalBuild extends Build {
   
-  lazy val buildVersion =  "2.0.3-08072012"
-  lazy val playVersion =  "2.1-08072012"
+  lazy val buildVersion =  "2.1-SNAPSHOT"
+  lazy val playVersion =  "2.1-RC1"
   
   lazy val typesafeSnapshot = "Typesafe Snapshots Repository" at "http://repo.typesafe.com/typesafe/snapshots/"
   lazy val typesafe = "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
@@ -13,16 +13,17 @@ object MinimalBuild extends Build {
   lazy val root = Project(id = "play-plugins-mailer", base = file("."), settings = Project.defaultSettings).settings(
     version := buildVersion,
     publishTo <<= (version) { version: String =>
-                val nexus = "http://repo.typesafe.com/typesafe/"
-                if (version.trim.endsWith("SNAPSHOT")) Some("snapshots" at nexus + "ivy-snapshots/") 
-                else                                   Some("releases"  at nexus + "ivy-releases/")
+                val nexus = "http://typesafe.artifactoryonline.com/typesafe/"
+                if (version.trim.endsWith("SNAPSHOT")) Some("snapshots" at nexus + "maven-snapshots/")
+                else                                   Some("releases"  at nexus + "maven-releases/")
     },
     organization := "com.typesafe",
+    scalaVersion := "2.10.0-RC1",
     resolvers += repo,
     javacOptions ++= Seq("-source","1.6","-target","1.6", "-encoding", "UTF-8"),
     javacOptions += "-Xlint:unchecked",
     libraryDependencies += "play" %% "play" % playVersion,
     libraryDependencies += "org.apache.commons" % "commons-email" % "1.2",
-    libraryDependencies += "com.typesafe" %% "play-plugins-util" % "2.0.3-08072012"
+    libraryDependencies += "com.typesafe" %% "play-plugins-util" % "2.1-SNAPSHOT"
   )
 }
--- util/project/Build.scala
+++ util/project/Build.scala
@@ -3,8 +3,8 @@ import Keys._
 
 object MinimalBuild extends Build {
   
-  lazy val buildVersion =  "2.1-09092012"
-  lazy val playVersion = "2.1-09092012"
+  lazy val buildVersion =  "2.1-SNAPSHOT"
+  lazy val playVersion = "2.1-RC1"
   lazy val typesafeSnapshot = "Typesafe Snapshots Repository" at "http://repo.typesafe.com/typesafe/snapshots/"
   lazy val typesafe = "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
   lazy val repo = if (buildVersion.endsWith("SNAPSHOT")) typesafeSnapshot else typesafe  
@@ -19,6 +19,7 @@ object MinimalBuild extends Build {
                 if (version.trim.endsWith("SNAPSHOT")) Some("snapshots" at nexus + "maven-snapshots/") 
                 else                                   Some("releases"  at nexus + "maven-releases/")
     },
+    scalaVersion := "2.10.0-RC1",
     organization := "com.typesafe",
     resolvers += repo,
     javacOptions += "-Xlint:unchecked",
