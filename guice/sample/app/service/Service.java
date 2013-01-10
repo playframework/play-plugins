bc61727477cba74982f9a705c100d7fc99dc7256
peter hausel <peter_kovac11@hotmail.com>
2013-01-10 11:23:34 -0500

    remove guice

--- guice/sample/app/service/Service.java
+++
@@ -1,6 +0,0 @@
-package service;
-
-public interface Service {
-
-  public String demonstrate();
-}
--- guice/sample/app/service/SomethingService.java
+++
@@ -1,9 +0,0 @@
-package service;
-
-public class SomethingService implements Service{
-
-  public String demonstrate() {
-    return "yay";
-  }
-
-}
