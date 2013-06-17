33165afac6d37d44393d3a4318b4ce48f4ee24b8
Robin Fernandes <rfernandes@atlassian.com>
2013-06-17 16:41:49 +1000

    Expose connection pool configuration. Update readme.

--- redis/project/Build.scala
+++ redis/project/Build.scala
@@ -10,7 +10,7 @@ object MinimalBuild extends Build {
   lazy val repo = if (buildVersion.endsWith("SNAPSHOT")) typesafeSnapshot else typesafe  
   lazy val pk11 = "pk11 repo" at "http://pk11-scratch.googlecode.com/svn/trunk"
   lazy val root = Project(id = "play-plugins-redis", base = file("."), settings = Project.defaultSettings).settings(
-    version := "2.1-1-RC2",
+    version := "2.1-1-RC2-robinf-3",
     scalaVersion := "2.10.0",
     publishTo <<= (version) { version: String =>
                 val nexus = "http://typesafe.artifactoryonline.com/typesafe/"
