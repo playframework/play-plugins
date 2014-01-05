589049528c37bc35d88454d721efa18f3f543629
Ahsan Rabbani <ahsan@xargsgrep.com>
2014-01-05 11:44:39 -0500

    added support for JedisSentinelPool

--- redis/project/Build.scala
+++ redis/project/Build.scala
@@ -3,7 +3,7 @@ import Keys._
 
 object MinimalBuild extends Build {
   
-  lazy val buildVersion =  "2.2.1"
+  lazy val buildVersion =  "2.2.0"
   
   lazy val typesafeSnapshot = "Typesafe Snapshots Repository" at "http://repo.typesafe.com/typesafe/snapshots/"
   lazy val typesafe = "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
