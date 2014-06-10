b13dc447a92f288c46299562c42e1cc75a380d78
Dylan Simon <dylan@dylex.net>
2014-06-09 20:08:52 -0400

    Update util and mailer to play 2.3, scala 2.11
    Also upgrade mailer to commons-email 1.3.2

--- mailer/project/Build.scala
+++ mailer/project/Build.scala
@@ -4,8 +4,8 @@ import Keys._
 object MinimalBuild extends Build {
   
 
-  lazy val buildVersion =  "2.2.0"
-  lazy val playVersion =  "2.2.0"
+  lazy val buildVersion =  "2.3.0"
+  lazy val playVersion =  "2.3.0"
   
   lazy val typesafeSnapshot = "Typesafe Snapshots Repository" at "http://repo.typesafe.com/typesafe/snapshots/"
   lazy val typesafe = "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/"
@@ -13,7 +13,7 @@ object MinimalBuild extends Build {
   
   lazy val root = Project(id = "play-plugins-mailer", base = file("."), settings = Project.defaultSettings).settings(
     version := buildVersion,
-    scalaVersion := "2.10.2",
+    scalaVersion := "2.11.1",
     publishTo <<= (version) { version: String =>
                 val nexus = "https://private-repo.typesafe.com/typesafe/"
                 if (version.trim.endsWith("SNAPSHOT")) Some("snapshots" at nexus + "maven-snapshots/")
@@ -24,7 +24,7 @@ object MinimalBuild extends Build {
     javacOptions ++= Seq("-source","1.6","-target","1.6", "-encoding", "UTF-8"),
     javacOptions += "-Xlint:unchecked",
     libraryDependencies += "com.typesafe.play" %% "play" % playVersion % "provided",
-    libraryDependencies += "org.apache.commons" % "commons-email" % "1.2",
+    libraryDependencies += "org.apache.commons" % "commons-email" % "1.3.2",
     libraryDependencies += "com.typesafe" %% "play-plugins-util" % buildVersion
   )
 }
